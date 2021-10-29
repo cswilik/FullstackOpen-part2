3d50623fd67bbca776755b4da9921a3126b0e2f1
CSWILIK <cswilik@gmail.com>
2021-10-29 16:15:48 -0400

    minor changes

--- src/Components/CountriesList.js
+++ src/Components/CountriesList.js
@@ -1,27 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import CountryItem from "./CountryItem";
 
 const CountriesList = ({countries}) => {
+    // const [details, setDetails] = useState(false)
 
-    
+
 
     let countryElements = countries.map(c => {
-        return <CountryItem name={c.name.common}/>
+            return  <CountryItem name={c.name.common} capital={c.capital}     
+            pop={c.population} langs={c.languages}
+            flag={c.flags.png} />
     })
-
     
-
-
-    return (
+    let len = countryElements.length  
+    if (len > 10) {
+        return <li>Too many Results, filter accordingly</li>
+    }
+    if (len === 1) {
+        return (
+            <div>
+                <h3>Countries</h3>
+                <ul>
+                    {countryElements}
+                </ul>
+            </div>
+        )
+    } else return (
         <div>
-            <h3>Countries</h3>
-            <ul>
-                {(countryElements.length > 10) ?
-                <li>Too many Results, filter accordingly</li> :
-                countryElements}
-                
-            </ul>
-        </div>
+                <h3>Countries</h3>
+                <ul>
+                    {countryElements}
+                </ul>
+            </div>
     )
 }
 
--- src/Components/CountryItem.js
+++ src/Components/CountryItem.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-const CountryItem= ({name}) => {
-    return (
-        <li>{name}</li>
-    )
+const CountryItem= ({name, capital, pop, langs, flag, details}) => {
+    
+    if (details) {
+        let languages = Object.entries(langs).map(l => {
+            return <li>{l[1]}</li>
+        })
+     
+        return (
+            <div>
+                <h1>{name}</h1>
+                <p><b>Capital:</b> {capital}</p>
+                <p><b>Population:</b> {pop}</p>
+                <p><b>Languages Spoken:</b></p>
+                <ul>
+                    {languages}
+                </ul>
+                <img src={flag} alt="flag"></img>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <li>{name} <button>Details</button></li>
+            </div>
+        )
+    }
 }
 
 export default CountryItem;
